204a10e16044a66a87a31d6d953cfdb2141c9544
Vinith Kumar <guntupallivineeth72@gmail.com>
2024-12-23 16:27:44 -0600

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    Version = '0.0.0',
+    author = 'Vinith Kumar',
+    author_email = 'vinithkumar72@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
